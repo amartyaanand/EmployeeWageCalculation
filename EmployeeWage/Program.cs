fe7b4729d5e0e3f86eadbd14c7efb3a6ebb59769
Amartya <amartyaanand21@live.com>
2020-09-28 21:30:14 +0530

    [Amartya] Add: Checking Employee Attendance.

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,7 +6,15 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation.");
+            int isPresent = 1;
+
+            Random random = new Random();
+            int checkVariable = random.Next(2);
+
+            if (checkVariable == isPresent)
+                Console.WriteLine("Employee is Present.");
+            else
+                Console.WriteLine("Employee is Absent.");
         }
     }
 }
